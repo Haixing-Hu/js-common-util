210ef73bfb45ee1bde56a960dab909a47994882a
Haixing Hu <starfish.hu@gmail.com>
2025-06-30 16:53:52 +0800

    fix: fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@
 module.exports = {
   verbose: true,
   testMatch: ['**/test/**/*.test.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-jsdom-global',
   testEnvironmentOptions: {
     customExportConditions: ['node', 'node-addons'],
   },
